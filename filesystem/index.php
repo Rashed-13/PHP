1125a8d1ba3d97ccd4473bc83bdb0c76bf7bbe49
Rashed <jmrashed.13@gmail.com>
2022-11-16 15:59:57 +0600

    initial commit

---
+++ filesystem/index.php
@@ -0,0 +1,39 @@
+<?php
+$filename = "\\Users\\jmras\\Desktop\\php\\filesystem\\file\\text.txt";
+$students = array(
+    array(
+        "fname" => "Shahin",
+        "lname" => "Ahmed",
+        "age"   => 12,
+        "class" => 7,
+        "roll"  => 11
+    ),
+    array(
+        "fname" => "Rahim",
+        "lname" => "Ahmed",
+        "age"   => 11,
+        "class" => 7,
+        "roll"  => 13
+    ),
+    array(
+        "fname" => "Nikhil",
+        "lname" => "Chandra",
+        "age"   => 12,
+        "class" => 7,
+        "roll"  => 14
+    ),
+    array(
+        "fname" => "Rashed",
+        "lname" => "Khan",
+        "age"   => 33,
+        "class" => 17,
+        "roll"  => 14
+    ),
+);
+
+
+
+
+
+
+?>
